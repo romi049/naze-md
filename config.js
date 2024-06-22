d8f1f46dfee7f8b399dc009c040d7c6289bd3361
romi049 <159658987+romi049@users.noreply.github.com>
2024-06-22 16:46:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['923256245454']
+global.ownernomer = "923256245454"
+global.premium = ['923256245454']
 global.packname = 'Sticker By'
 global.author = 'Naze Dev'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
